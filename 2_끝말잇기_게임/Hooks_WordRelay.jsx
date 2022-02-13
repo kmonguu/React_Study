a81d38730b5494a25add270f681432e87ba67163
kmonguu <50946547+kmonguu@users.noreply.github.com>
2022-02-13 23:34:28 +0900

    feat: 끝말잇기 프로젝트 추가

---
+++ 2_끝말잇기_게임/Hooks_WordRelay.jsx
@@ -0,0 +1,40 @@
+const React = require('react');
+const { useState, useRef } = React;
+
+const WordRelay = () => {
+    const [word, setWord] = useState('끝말잇기 시작');
+    const [value, setValue] = useState('');
+    const [result, setResult] = useState('');
+    const inputRef = useReft(null);
+};
+
+const onSubmitForm = (e) => {
+    e.preventDefault();
+    if (word[word.length - 1] === value[0]) {
+        setResult ('정답');
+        setWord (value);
+        setValue ('');
+        inputRef.current.focus();
+    } else {
+        setResult ('다시 입력하세요');
+        setValue ('');
+        inputRef.current.focus();
+    }
+};
+
+const onChangeInput = (e) => {
+    setValue(e.target.value);
+};
+
+return (
+    <>
+        <div>{word}</div>
+        <form onSubmit={onSubmitForm}>
+            <input ref={onRefInput} value={value} onChange={onChangeInput} />
+            <button>결과</button>
+        </form>
+        <div>{result}</div>
+    </>
+);
+
+module.exports = WordRelay;
---
+++ 2_끝말잇기_게임/client.jsx
@@ -0,0 +1,6 @@
+const React = require('react');
+const ReactDom = require('react-dom');
+
+const WordRelay = require('./Class_WordRelay');
+
+ReactDom.render(<Class_WordRelay />, document.querySelector('#root'));
